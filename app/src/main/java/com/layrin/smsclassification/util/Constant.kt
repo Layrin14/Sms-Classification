10958497f3e2299313aafb41e436e311a55a5a53
Layrin14 <john.lawlet13@gmail.com>
2022-10-17 07:37:01 -0700

    initial commit

--- app/src/main/java/com/layrin/smsclassification/util/Constant.kt
+++ app/src/main/java/com/layrin/smsclassification/util/Constant.kt
@@ -3,7 +3,5 @@ package com.layrin.smsclassification.util
 class Constant {
     companion object {
         const val DATABASE_NAME = "sms-database"
-
-        const val KEY_RESUME_DATE = "last_date"
     }
 }
